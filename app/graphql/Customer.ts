68864beca4e673daeeb5a3c693e67980278a6525
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-20 21:24:57 -0700

    create createCustomer and getCustomer apis.

--- app/graphql/Customer.ts
+++ app/graphql/Customer.ts
@@ -1,30 +1,68 @@
-import { extendType, nonNull, objectType, stringArg } from "nexus";
+import { extendType, nonNull, objectType, queryField, queryType, stringArg } from "nexus";
 // src
-import { NexusGenObjects } from "../../nexus-typegen";  
+import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Customer = objectType({
-    name: "Customer",
-    definition(t) {
-        t.nonNull.int("id");
-        t.nonNull.string("slug");
-        t.nonNull.string("createdAt");
-    },
+  name: "Customer",
+  definition(t) {
+    t.nonNull.int("id");
+    t.string("slug");
+    t.dateTime("createdAt");
+    t.list.field('users', {
+      type: "User",
+      resolve(root, args, ctx) {
+        return ctx.prisma.user.findMany({ where: {
+          customerId: root.id
+        }})
+      },
+    })
+    // t.list.field('databases', {
+    //   type: "DBCredentials",
+    //   resolve(root, args, ctx) {
+    //     return ctx.prisma.dBCredentials.findMany({ where: {
+    //       customerId: root.id
+    //     }})
+    //   },
+    // })
+  },
 });
 
 export const CustomerMutation = extendType({
-    type: "Mutation",
-    definition(t) {
-        t.nonNull.field("createCustomer", { 
-            type: "Customer",
-            args: {
-                slug: nonNull(stringArg()),
-            },
-            async resolve(parent, args, context) {
-                const  { slug } = args;
-                const customer = await context.prisma.customer.create({
-                    data: { slug },
-                });
-                return customer;
-            }})
-    }
-})
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("createCustomer", {
+      type: "Customer",
+      args: {
+        slug: nonNull(stringArg()),
+      },
+      async resolve(parent, args, context) {
+        const { slug } = args;
+        const customer = await context.prisma.customer.create({
+          data: { slug },
+        });
+        return customer;
+      },
+    });
+  },
+});
+
+export const CustomerQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.field("getCustomer", {
+      type: "Customer",
+      args: {
+        slug: stringArg(),
+      },
+      async resolve(parent, args, context, info) {
+        const { slug } = args;
+        const customer = await context.prisma.customer.findUniqueOrThrow({
+            where: {
+                slug: slug ?? undefined,
+            }
+        })
+        return customer;
+      },
+    });
+  },
+});
