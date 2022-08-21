5c8c1b8ce5f0aaa5bdcf1c8b18966374a5f35b46
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-20 23:24:27 -0700

    create UserRole in graphql type

--- app/graphql/User.ts
+++ app/graphql/User.ts
@@ -1,7 +1,14 @@
-import { objectType, extendType, nonNull, stringArg } from "nexus";
+import { objectType, extendType, nonNull, stringArg, enumType } from "nexus";
 // src
 import { NexusGenObjects } from "../../nexus-typegen";  
 
+export const UserRole = enumType({
+    name: 'UserRole',
+    members: ['ADMIN', 'OPERATOR'],
+    description: 'Roles that determine what permissions the user has access to.',
+  })
+
+  
 export const User = objectType({
     name: "User",
     definition(t) {
@@ -9,10 +16,13 @@ export const User = objectType({
         t.nonNull.string("firstName");
         t.nonNull.string("lastName");
         t.nonNull.string("email");
+        t.nonNull.field("role", {
+            type: "UserRole",
+        })
     },
 });
 
-export const UserQuery = extendType({ 
+export const UserQuery = extendType({
     type: "Query",
     definition(t) {
         t.nonNull.list.nonNull.field("getUsers", {
