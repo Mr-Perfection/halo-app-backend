f34a23dcb53f3d7f1e55d242bc92129905bddcc0
Stephen Lee <sungsoolee0127@gmail.com>
2022-09-02 13:58:30 -0700

    add INVALID_CUSTOMER_ERROR error

--- app/constants/auth.ts
+++ app/constants/auth.ts
@@ -1,7 +1,8 @@
-import { AuthenticationError } from 'apollo-server-core';
+import { AuthenticationError, ValidationError } from 'apollo-server-core';
 
 export const CustomGraphQLErrors = {
-    AUTH_ERROR: new AuthenticationError("User must be authenticated.")
+    AUTH_ERROR: new AuthenticationError("User must be authenticated."),
+    INVALID_CUSTOMER_ERROR: new ValidationError("Customer does not exist or is invalid.")
 }
 
 export const JWTTokenFields = {
